8a339191f05f35b1bd9d6a367401d31a1f94e656
Anton Shefer <rezajkee@mail.ru>
2022-04-10 20:57:41 +0500

    Step 1: make environment, trying argparse

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import argparse
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+args = parser.parse_args()
+
+
+
+def main():
+    print(args.indir)
+
+
+if __name__ == '__main__':
+    main()
