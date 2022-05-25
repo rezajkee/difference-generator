5599eb04909e2abd0de2eed45cdb87d24f8dc9cb
Anton Shefer <rezajkee@mail.ru>
2022-05-25 13:16:38 +0500

    Refactoring plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,56 +1,48 @@
-def plain(source, path=[], depth=1):
-    result = []
+def plain(source, path="", depth=1):
+    children = source.get("children")
+    formatted_value = format_value(source.get("value"))
+    formatted_value1 = format_value(source.get("value1"))
+    formatted_value2 = format_value(source.get("value2"))
 
     if source["type"] == "root":
-        for item in source["children"]:
-            result.append(plain(item))
-
-    elif source["type"] == "nested":
-        path.append(source["key"])
-        for item in source["children"]:
-            result.append(plain(item, path, depth + 1))
-        del path[-1::depth]
-
-    elif source["type"] == "added":
-        path.append(source["key"])
-        result.append(
-            "Property '{}' was added with value: {}".format(
-                ".".join(path), format_value(source["value"])
-            )
+        lines = map(plain, children)
+        lines = filter(None, lines)
+        return "\n".join(lines)
+    if source["type"] == "nested":
+        nested_path = build_path(source, path)
+        lines = map(
+            lambda child: plain(child, nested_path, depth + 1), children
         )
-        del path[-1::depth]
-
-    elif source["type"] == "removed":
-        path.append(source["key"])
-        result.append("Property '{}' was removed".format(".".join(path)))
-        del path[-1::depth]
-
-    elif source["type"] == "modified":
-        path.append(source["key"])
-        result.append(
-            "Property '{}' was updated. From {} to {}".format(
-                ".".join(path),
-                format_value(source["value1"]),
-                format_value(source["value2"]),
-            )
+        lines = filter(None, lines)
+        return "\n".join(lines)
+    if source["type"] == "added":
+        return "Property '{0}' was added with value: {1}".format(
+            build_path(source, path), formatted_value
         )
-        del path[-1::depth]
-
-    result = filter(bool, result)
-    return "\n".join(result)
+    if source["type"] == "removed":
+        return "Property '{}' was removed".format(build_path(source, path))
+    if source["type"] == "modified":
+        return "Property '{0}' was updated. From {1} to {2}".format(
+            build_path(source, path),
+            formatted_value1,
+            formatted_value2,
+        )
 
 
 def format_value(value):
     if isinstance(value, bool):
         return "true" if value else "false"
-
     if value is None:
         return "null"
-
     if isinstance(value, dict):
         return "[complex value]"
-
     if value == 0:
         return "0"
-
     return "'{}'".format(value)  # for other data types
+
+
+def build_path(source, path):
+    if path:
+        return ".".join([path, source["key"]])
+    else:
+        return source["key"]
