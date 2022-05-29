9092ce7802e3a99a9b09d28ea7f41c6c7207aa84
Anton Shefer <rezajkee@mail.ru>
2022-05-29 12:55:52 +0500

    Remove else block in parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -15,10 +15,9 @@ def parse(data, format_name):
         return json.load(data)
     elif format_name in ["yaml", "yml", "YML"]:
         return yaml.safe_load(data)
-    else:
-        raise ValueError(
-            """"{}" file format not found.
-            Format should be "json", "yaml" or "yml".""".format(
-                format_name
-            )
+    raise ValueError(
+        """"{}" file format not found.
+        Format should be "json", "yaml" or "yml".""".format(
+            format_name
         )
+    )
